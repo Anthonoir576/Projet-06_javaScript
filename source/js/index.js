914f537f1e25c199f6c704859a15b6fa6eea8a7c
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-15 04:32:39 +0200

    probleme resolu

--- source/js/index.js
+++ source/js/index.js
@@ -1,28 +1,33 @@
+(function() {
 
-changeBackground();
-
-function changeBackground() {
-
+    // CONSTANTE ET VARIABLE
     const colonne1 = aleatoireRGB();
     const colonne2 = aleatoireRGB();
     const colonne3 = aleatoireRGB();
 
-    const couleurCaractere = `rgb(${colonne1}, ${colonne2}, ${colonne3})`
+    const couleurCaractere = `rgb(${colonne1}, ${colonne2}, ${colonne3})`;
 
 
-}
+    // FONCTIONS
+    function changeBackground() {
+
+        document.getElementById('affichage').style.background = couleurCaractere;
+
+    };
+
+
+    function aleatoireRGB() {
+
+        return Math.floor(Math.random() * 256);
+
+    };
+
+
+    changeBackground()
+
+})()
 
 
 
-function aleatoireRGB() {
-
-    const pars = parseInt('Math.floor(Math.random * 256)', 10);
-
-    return pars;
-
-};
-
-console.log(aleatoireRGB())
 
 
-// REVOIR LE CODE
