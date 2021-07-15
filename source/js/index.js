9865c2bdfd45925838c73e109c927b2ba2516d53
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-15 05:50:56 +0200

    probleme resolu

--- source/js/index.js
+++ source/js/index.js
@@ -1,32 +1,37 @@
 (function() {
 
-    // CONSTANTE ET VARIABLE
-    const colonne1 = aleatoireRGB();
-    const colonne2 = aleatoireRGB();
-    const colonne3 = aleatoireRGB();
 
-    const couleurCaractere = `rgb(${colonne1}, ${colonne2}, ${colonne3})`;
+    // CONSTANTE ET VARIABLE
+    const button = document.getElementById('color');
+
 
 
     // FONCTIONS
-    function changeBackground() {
-
-        document.getElementById('affichage').style.background = couleurCaractere;
-
-    };
-
-
     function aleatoireRGB() {
-
+    
         return Math.floor(Math.random() * 256);
 
     };
+    
+    
+    function changeBackground() {
 
+        const colonne1 = aleatoireRGB();
+        const colonne2 = aleatoireRGB();
+        const colonne3 = aleatoireRGB();
+        
+        const couleurCaractere = `rgb(${colonne1}, ${colonne2}, ${colonne3})`;
 
-    changeBackground()
+        document.getElementById('affichage').style.background = couleurCaractere;
+        document.getElementById('color').innerHTML = couleurCaractere;
 
+    };
+
+    button.addEventListener('click', changeBackground);
+    
 })()
 
+    
 
 
 
